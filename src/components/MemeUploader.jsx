45b032227136fd43b30a7b16f322a5a55b53aa14
rabbykhairul <rabbykhairul@gmail.com>
2021-09-04 20:36:07 +0600

    'MemeUploader' - add state & methods to handle user input

--- src/components/MemeUploader.jsx
+++ src/components/MemeUploader.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./commons/Button";
 import FileInput from "./commons/FileInput";
 import TextInput from "./commons/TextInput";
 
 const MemeUploader = () => {
+  const [ memeLink, setMemeLink ] = useState("");
+
+  const handleMemeLinkInput = (e) => {
+    console.log("link: ", e.target.value);
+    setMemeLink(e.target.value);
+  };
+
   return (
     <div className="meme-uploader-container">
       <div className="meme-link-upload">
-        <TextInput className="link-input" placeholder="Paste a link here.." />
+        <TextInput 
+          className="link-input" 
+          placeholder="Paste a link here.."
+          value={memeLink}
+          onChange={handleMemeLinkInput} 
+        />
         <Button className="round-button" >Add meme</Button>
       </div>
 
