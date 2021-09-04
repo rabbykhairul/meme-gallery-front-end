1dc792a12548b0537f5fab18c08550c80dfce461
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 02:01:18 +0600

    'MemeUploader' - add the facility to display notifications to the user

--- src/components/MemeUploader.jsx
+++ src/components/MemeUploader.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./commons/Button";
 import FileInput from "./commons/FileInput";
+import NotificationCard from "./commons/NotificationCard";
 import TextInput from "./commons/TextInput";
+
+const UPLOAD_NOTIFICATION_INFO = {
+  inProgress: {
+    type: "loading",
+    message: "Uploading your meme"
+  },
+
+  success: {
+    type: "success",
+    message: "Upload successful. Scroll to top to see your new meme."
+  },
+
+  error: {
+    type: "error",
+    message: "Upload failed. Please try again."
+  }
+};
 
 const MemeUploader = () => {
   const [ memeLink, setMemeLink ] = useState("");
   const [ memeFile, setMemeFile ] = useState(null);
+
+  const [ shouldDisplayNotification, setShouldDisplayNotification ] = useState(false);
+  const [ notificationInfo, setNotificationInfo ] = useState(UPLOAD_NOTIFICATION_INFO.inProgress);
 
   const handleMemeLinkInput = (e) => {
     console.log("link: ", e.target.value);
@@ -35,6 +56,14 @@ const MemeUploader = () => {
         accept="image/*"
         onChange={handleMemeFileSelection} 
       />
+
+      {
+        shouldDisplayNotification && 
+        <NotificationCard
+          type={notificationInfo.type}
+          message={notificationInfo.message} 
+        />
+      }
     </div>
   );
 };
