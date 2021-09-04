93252f912c61b36766732af77b2b7eedc2d710b6
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 05:55:07 +0600

    refactor code

--- src/services/memeService.js
+++ src/services/memeService.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
+const MEME_API_URL = `${process.env.REACT_APP_API_BASE_URL}/memes`;
+
 export const uploadMeme = async (memeUrl, memeFile) => {
   const formData = new FormData();
   formData.append("url", memeUrl);
   if (memeFile) formData.append("memeFile", memeFile, memeFile?.name);
   
   try {
-    const url = `${process.env.REACT_APP_API_BASE_URL}/memes`;
-    const { data } = await axios.post(url, formData);
+    const { data } = await axios.post(MEME_API_URL, formData);
     return data;
   } catch (err) {
     return { success: false };
   }
-}
+};
 
 const memeService = { uploadMeme };
 export default memeService;
