136d51599c0bd2b1db9bdafce583f02d8d1a63b5
rabbykhairul <rabbykhairul@gmail.com>
2021-09-04 20:35:24 +0600

    transform 'TextInput' into a controlled component

--- src/components/commons/TextInput.jsx
+++ src/components/commons/TextInput.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
 const TextInput = (props) => {
-  const { type = "text", placeholder, className = "" } = props;
+  const { 
+    type = "text",
+    placeholder, 
+    className = "",
+    value = "",
+    onChange
+  } = props;
 
   return (
-    <input type={type} placeholder={placeholder} className={className} />
+    <input 
+      type={type} 
+      placeholder={placeholder} 
+      className={className}
+      value={value}
+      onChange={onChange}
+    />
   )
 };
 
