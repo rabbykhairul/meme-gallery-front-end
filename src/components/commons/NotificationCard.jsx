923fc3a5952cab2db73922d7f582592936e4c6e5
rabbykhairul <rabbykhairul@gmail.com>
2021-09-04 21:59:33 +0600

    automatically dismiss notifications after 8.5s

--- src/components/commons/NotificationCard.jsx
+++ src/components/commons/NotificationCard.jsx
@@ -1,13 +1,22 @@
 import React, { createRef, useEffect } from "react";
 
+let timer = null;
+
 const NotificationCard = (props) => {
   const { message = "", type = "loading" } = props;
 
   const cardRef = createRef();
 
   useEffect(() => {
-    cardRef.current.classList.add("display-notification", "notification-" + type);
+    cardRef.current.classList.add("display-notification", "notification-" + props.type);
   }, [props.message, props.type]);
+
+  useEffect(() => {
+    if (props.type === "success" || props.type === "error") {
+      clearTimeout(timer);
+      timer = setTimeout(() => hideNotification(), 8500);
+    }
+  }, [props.type]);
 
   const hideNotification = () => cardRef.current.classList.remove("display-notification");
 
