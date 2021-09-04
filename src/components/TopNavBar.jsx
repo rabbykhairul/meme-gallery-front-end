a0697b8ad47e908fc502da1ec61442ac4d112592
rabbykhairul <rabbykhairul@gmail.com>
2021-09-04 19:22:00 +0600

    display 'MemeUploader' inside 'TopNavBar' component

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "./commons/Button";
+import MemeUploader from "./MemeUploader";
 
 const TopNavBar = () => {
   return(
     <nav>
       <header>Meme Gallery</header>
       <Button>See Stats</Button>
+      <MemeUploader />
     </nav>
   )
 };
