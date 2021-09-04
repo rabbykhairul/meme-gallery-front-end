e7a41f71838b07ba61a0d0863cea4f1f259e1107
rabbykhairul <rabbykhairul@gmail.com>
2021-09-04 20:42:54 +0600

    transform 'FileInput' into a controlled component

--- src/components/commons/FileInput.jsx
+++ src/components/commons/FileInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const FileInput = (props) => {
-  const { id = "upload", displayTitle = "Upload", accept = "*" } = props;
+  const { id = "upload", displayTitle = "Upload", accept = "*", onChange } = props;
   return (
     <>
-      <input type="file" id={id} accept={accept} hidden />
+      <input type="file" id={id} accept={accept} onChange={onChange} hidden />
       <label htmlFor={id}>
         <span className="round-button file-upload" >{displayTitle}</span>
       </label>
