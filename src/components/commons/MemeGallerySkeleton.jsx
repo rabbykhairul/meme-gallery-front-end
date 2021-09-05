ee21811cf03eb13c72a584a0ffd562cf9dbff586
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 15:11:51 +0600

    create 'MemeGallerySkeleton' component to display loading on progress on page load

---
+++ src/components/commons/MemeGallerySkeleton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ContentLoadingCard from "./ContentLoadingCard";
+
+const MemeGallerySkeleton = (props) => {
+  const { memeIds } = props;
+
+  return (
+    <div className="meme-gallery">
+      {[1, 2, 3].map( id => <div key={id} className="meme-card"> <ContentLoadingCard /> </div>)}
+    </div>
+  );
+};
+
+export default MemeGallerySkeleton;
