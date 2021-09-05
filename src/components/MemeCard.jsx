91adf763fc6662ead47255ee6eca1be0e16a4c02
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 15:01:36 +0600

    'MemeCard' - load meme via api

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getMemeById } from "../services/memeService";
 import ContentLoadingCard from "./commons/ContentLoadingCard";
+import Button from "./commons/Button";
 
 const MemeCard = (props) => {
   const { memeId, onDelete } = props;
   const [ showLoadingSkeleton, setShowLoadingSkeleton ] = useState(true);
+  const [ meme, setMeme ] = useState({});
+
+  useEffect(() => loadMeme(), []);
+
+  const loadMeme = async () => {
+    const { success, meme } = await getMemeById(memeId);
+
+    if (success) {
+      setMeme(meme);
+      setShowLoadingSkeleton(false);
+    }
+  }
 
   const renderLoadingSkeleton = () => <ContentLoadingCard />;
+
+  const renderMemeCardContent = () => {
+    return (
+      <>
+        <div className="meme-image">
+          <img src={meme.url || meme.base64Image} />
+        </div>
+        <div className="meme-action">
+          <Button onClick={() => {}} >Delete</Button>
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className="meme-card">
       { showLoadingSkeleton && renderLoadingSkeleton() }
+      { !showLoadingSkeleton && renderMemeCardContent() }
     </div>
   );
 };
