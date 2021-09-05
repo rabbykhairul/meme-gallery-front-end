4b7a23745aa1494262a652d169263c2ade7c37a6
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 15:17:30 +0600

    display uploaded memes on page load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import MemeGallerySkeleton from './components/commons/MemeGallerySkeleton';
+import MemeGallery from './components/MemeGallery';
 import TopNavBar from "./components/TopNavBar";
+import { getMemes } from './services/memeService';
 
 function App() {
+  const [ memeIds, setMemeIds ] = useState([]);
+  const [ isMemeIdsLoading, setIsMemeIdsLoading ] = useState(true);
+
+  useEffect(() => loadMemeIds(), []);
+
+  const loadMemeIds = async() => {
+    const { success, memes = [] } = await getMemes();
+    if (success) setMemeIds(memes);
+    setIsMemeIdsLoading(false);
+  };
+
   return (
-    <TopNavBar />
+    <>
+      <TopNavBar />
+      { isMemeIdsLoading && <MemeGallerySkeleton /> }
+      { !isMemeIdsLoading && <MemeGallery memeIds={memeIds} /> }
+    </>
   );
 }
 
