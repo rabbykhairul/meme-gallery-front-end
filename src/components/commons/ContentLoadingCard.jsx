aafc6e2ccb93545cef1a8ad44b9d3bb63940617b
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 13:21:22 +0600

    add 'ContentLoadingCard' component to display as loading skeleton

---
+++ src/components/commons/ContentLoadingCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ContentLoadingCard = () => {
+  return (
+    <div className="content-loading-card">
+      <div className="content-part">
+        <div className="content-line"></div>
+        <div className="content-line line-one-third"></div>
+        <div className="content-line line-half"></div>
+      </div>
+      <div className="action-part"></div>
+    </div>
+  );
+};
+
+export default ContentLoadingCard;
